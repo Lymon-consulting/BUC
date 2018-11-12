09c0f997b24150d2f8d65ff77ecd0532a7d63f58
zenen.morales <zenen.morales@globant.com>
2018-11-12 08:10:35 -0600

    BUC-01: Estructura inicial del proyecto.

---
+++ src/main/java/com/banorte/buc/BucApplication.java
@@ -0,0 +1,12 @@
+package com.banorte.buc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucApplication.class, args);
+	}
+}
